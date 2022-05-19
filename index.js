601e22ae4f61bbe7c8d01d5b668abcf65efde3fa
Moringa <alinoorshukri8@gmail.com>
2022-05-19 15:49:53 +0300

    Mentor Signup

--- index.js
+++
@@ -1,8 +0,0 @@
-function openUser(userName) {
- var i;
- var x = document.getElementsByClassName("user");
- for (i = 0; i < x.length; i++) {
-   x[i].style.display = "none";
- }
- document.getElementById(userName).style.display = "block";
-}
